97dac0ad4f3d504423a47c05bbf95203857f98c9
japonesmatdor <joao.goncalves.cunha@escola.pr.gov.br>
2025-08-14 09:04:59 -0300

    Add files via upload

---
+++ domicio/land.js
@@ -0,0 +1,21 @@
+document.getElementById('contactForm').addEventListener('submit', function (event){EventTarget.preventDefault();
+    const formData = new FormData(event.target);
+    const data = {};
+    formData.forEach((value, key)=> {
+        data[key] = value;
+    }); 
+
+    console.log(data);
+
+    fetch('SEU LINK DA PLANILHA DO GOOGLE', {
+        method: 'POST',
+        body: new  URLSearchParams(data)
+    })
+    .then(response => response.json())
+    .then(responseData => {
+        if (responseData.result === 'succes'){
+            alert ('Dados enviados com sucesso!');
+            event.target.reset();
+        }else if (responseData.result === 'rror'&& responseData.message === 'Email already exists'){}
+    })
+})
